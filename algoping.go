80f2478f588fd3f1b350af24b554fa73ccaafbc8
Bastin Robin <bastinrobins@gmail.com>
2021-10-29 22:12:59 +0530

    Initial commit

---
+++ algoping.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	
+	// Get list of arguments from the command prompt 
+	// eg: algoping language, filepath, stdin, stdout
+	// expected return output, time, memory
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bastinrobin/algoping
+
+go 1.17
