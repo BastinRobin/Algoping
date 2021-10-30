fcaa50c24f30f3e1aac25ab3663b97080b020f89
Bastin Robin <bastinrobins@gmail.com>
2021-10-30 11:31:15 +0530

    Fix: Stdin, Stdout, Stderr redirection

--- algoping.go
+++ algoping.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/bastinrobin/algoping/compile"
 )
 
@@ -10,13 +9,10 @@ func main() {
 	// Get list of arguments from the command prompt 
 	// eg: algoping language, filepath, stdin, stdout
 	// expected return output, time, memory
-	file := "/Users/developer/Desktop/trial/test.py"
-	inFile := "</Users/developer/Desktop/trial/input.txt"
-	outFile := "1>/Users/developer/Desktop/trial/output.txt"
-	errFile := "2>/Users/developer/Desktop/trial/error.txt"
-	lang := "/usr/bin/python"
-	result, elapsed, memory := compile.Sandbox(lang, file, inFile, outFile, errFile)
-	fmt.Println("Result: ", result)
-	fmt.Println("Time: ", elapsed)
-	fmt.Println("Mem: ", memory)
+	file := "/Users/developer/Desktop/trial/com.py"
+	inFile := "/Users/developer/Desktop/trial/input.txt"
+	outFile := "/Users/developer/Desktop/trial/output.txt"
+	errFile := "/Users/developer/Desktop/trial/error.txt"
+	lang := "/usr/local/bin/python3"
+	compile.Sandbox(lang, file, inFile, outFile, errFile)
 }
--- compile/compile.go
+++ compile/compile.go
@@ -1,18 +1,44 @@
 package compile
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
+    "os"
+    "log"
+    "fmt"
+    "bufio"
+    "os/exec"
 )
 
-// Sandbox executes the code and returns output 
-func Sandbox(lang string, path string, inFile string, outFile string, errFile string) (string, string, string){
+// Execute the command from sandbox
+func Sandbox(lang string, path string, inFile string, outFile string, errFile string) {
+       
+           inputReader, err := os.Open(inFile)
+           if err != nil {
+                log.Fatal(err) 
+           }
+           
+           
+           cmd := exec.Command(lang, path)
+           cmd.Stdin = bufio.NewReader(inputReader) 
 
-	filePath := fmt.Sprintf("%v <%v", path, inFile)
-	cmd := exec.Command(lang, filePath)
-	cmd.Stderr := os.Stderr
-	return "Finished", "time", "mem"
+           // open the out file for redirecting standard output
+           outfile, err := os.Create(outFile)
+           if err != nil {
+             panic(err)
+           }
+           defer outfile.Close()
+           cmd.Stdout = outfile
+           
+           // open the error file for redirecting standard error
+           errorfile, err := os.Create(errFile)
+           if err != nil {
+             panic(err)
+           }
+           defer errorfile.Close()
+           cmd.Stderr = errorfile
+
+           err = cmd.Run()
+           if err != nil {
+            fmt.Printf("Error: %v", err)
+            os.Exit(1)
+           }
 }
-
-
